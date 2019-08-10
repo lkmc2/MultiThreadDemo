c03feda3c53d55117f69702777f40df43c4c8c1a
lkmc2 <lkmc2@163.com>
2019-08-10 16:53:06 +0800

    添加在没有同步的情况下统计已处理请求数量的Servlet（不要这样做）

--- src/main/java/com/lin/ch01/UnsafeCountingFactorizer.java
+++ src/main/java/com/lin/ch01/UnsafeCountingFactorizer.java
@@ -1,5 +1,6 @@
 package com.lin.ch01;
 
+import com.lin.annotion.NotThreadSafe;
 import com.lin.annotion.ThreadSafe;
 import com.lin.ch01.servlet.Servlet;
 import com.lin.ch01.servlet.ServletRequest;
@@ -9,15 +10,24 @@ import java.math.BigInteger;
 import java.util.Arrays;
 
 /**
- * 无状态的Servlet（无状态的对象是线程安全的）
+ * 在没有同步的情况下统计已处理请求数量的Servlet（不要这样做）
  * @author lkmc2
  * @date 2019/8/10 16:37
  */
-@ThreadSafe
-public class StatelessFactorizer implements Servlet {
+@NotThreadSafe
+public class UnsafeCountingFactorizer implements Servlet {
+
+    private long count = 0;
+
+    public long getCount() {
+        return count;
+    }
+
     public void service(ServletRequest request, ServletResponse response) {
         BigInteger i = extractFromRequest(request);
         BigInteger[] factors = factor(i);
+        // 此处的自增会被分成 取值、增加、赋值 三个步骤，当被多个线程访问时时不安全的
+        count++;
         encodeIntoResponse(response, factors);
     }
 
