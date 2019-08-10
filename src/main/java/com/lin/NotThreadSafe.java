b31d0782a67407470334d3287fb95fb0ed39b442
lkmc2 <lkmc2@163.com>
2019-08-10 16:29:36 +0800

    添加非线程安全的序列生成器

---
+++ src/main/java/com/lin/NotThreadSafe.java
@@ -0,0 +1,9 @@
+package com.lin;
+
+/**
+ * 非线程安全注解
+ * @author lkmc2
+ * @date 2019/8/10 16:16
+ */
+public @interface NotThreadSafe {
+}
---
+++ src/main/java/com/lin/ch01/UnsafeSequence.java
@@ -0,0 +1,18 @@
+package com.lin.ch01;
+
+import com.lin.NotThreadSafe;
+
+/**
+ * 非线程安全的序列生成器
+ * @author lkmc2
+ * @date 2019/8/10 16:15
+ */
+@NotThreadSafe
+public class UnsafeSequence {
+    private int value;
+
+    /** 返回一个唯一的数值 **/
+    public int getNext() {
+        return value++;
+    }
+}
